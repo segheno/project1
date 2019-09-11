9cb7c41428aa75c5776c0d1f79582ad1f14218f0
Olusegun Oye <38997140+segheno@users.noreply.github.com>
2019-09-11 04:48:15 +0100

    Update setenvars.sh

--- setenvars.sh
+++ setenvars.sh
@@ -1,5 +1,3 @@
 export DATABASE_URL=postgres://segheno:Princess13@localhost:5432/mydb
 export FLASK_DEBUG=1
 export FLASK_APP=application.py
-export DATABASE_URL=postgres://segheno:@localhost:5432/project
-export DATABASE_URL=postgres:///project
